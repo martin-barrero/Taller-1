3dca7ab84f935c4188cd3d143913092f95ec1485
JuanF-Cano <juan.cano5@utp.edu.co>
2025-02-18 10:51:28 -0500

    primer punto, faltan primos y tios

---
+++ punto-1.pl
@@ -0,0 +1,56 @@
+%Hechos.
+padre_de(abraham, herbert).
+padre_de(abraham, homero).
+padre_de(clancy, marge).
+padre_de(clancy, patty).
+padre_de(clancy, selma).
+padre_de(homero, bart).
+padre_de(homero, lisa).
+padre_de(homero, maggie).
+
+madre_de(mona, homero).
+madre_de(jacqueline, marge).
+madre_de(jacqueline, patty).
+madre_de(jacqueline, selma).
+madre_de(marge, bart).
+madre_de(marge, lisa).
+madre_de(marge, maggie).
+madre_de(selma, ling).
+
+es_mujer(mona).
+es_mujer(jacqueline).
+es_mujer(marge).
+es_mujer(patty).
+es_mujer(selma).
+es_mujer(lisa).
+es_mujer(maggie).
+es_mujer(ling).
+
+es_hombre(abraham).
+es_hombre(clancy).
+es_hombre(herbert).
+es_hombre(homero).
+es_hombre(bart).
+
+%Reglas.
+abuelo_de(X, Y):-
+    es_hombre(X),
+    padre_de(X, Z),
+    (padre_de(Z, Y); madre_de(Z, Y)).
+
+abuela_de(X, Y):-
+    es_mujer(X),
+    madre_de(X, Z),
+    (padre_de(Z, Y); madre_de(Z, Y)).
+
+hermano_de(X, Y) :-
+    es_hombre(X),
+    ((padre_de(Z, X), padre_de(Z, Y)), (madre_de(W, X), madre_de(W, Y));
+    (padre_de(Z, X), padre_de(Z, Y)); (madre_de(W, X), madre_de(W, Y))),
+    X \= Y.
+
+hermana_de(X, Y) :-
+    es_mujer(X),
+    ((padre_de(Z, X), padre_de(Z, Y)), (madre_de(W, X), madre_de(W, Y));
+    (padre_de(Z, X), padre_de(Z, Y)); (madre_de(W, X), madre_de(W, Y))),
+    X \= Y.
