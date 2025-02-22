0b67babe223e27d49999a53ad80fa50a003eb1e2
AGBR121 <angel.burbano@utp.edu.co>
2025-02-22 13:09:48 -0500

    point 2 fixed

--- punto-2.pl
+++ punto-2.pl
@@ -1,6 +1,7 @@
-es_estadounidense(Coronel_West).
-enemigo_de(Usa, Corea_sur).
-tiene(Corea_sur, misiles).
-vendio(Coronel_West, misiles, Corea_sur).
+%Hechos
+estadounidense(coronel_west).
+nacion_hostil(corea_sur).
+vendio(coronel_west, misil, corea_sur).
 
-% Hechos creados segun logica de predicados
+%Reglas
+criminal(X) :- estadounidense(X), nacion_hostil(Y), vendio(X, misil, Y).
