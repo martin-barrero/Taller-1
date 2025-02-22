a847ab92133e211d454bee45fb20747f9f1a47fb
AGBR121 <angel.burbano@utp.edu.co>
2025-02-22 13:26:05 -0500

    explanation point 2

--- punto-2.pl
+++ punto-2.pl
@@ -1,7 +1,8 @@
-%Hechos
-estadounidense(coronel_west).
-nacion_hostil(corea_sur).
-vendio(coronel_west, misil, corea_sur).
+%Hechos.
+estadounidense(coronel_west). %El coronel West es estadounidense.
+nacion_hostil(corea_sur). %Corea del Sur es una nación hostil.
+arma(misil). %Un misil es un arma.
+vendio(coronel_west, misil, corea_sur). %El coronel West vendió un misil a Corea del Sur.
 
-%Reglas
-criminal(X) :- estadounidense(X), nacion_hostil(Y), vendio(X, misil, Y).
+%Reglas.
+criminal(X) :- estadounidense(X), nacion_hostil(Y), arma(Z), vendio(X, Z, Y). %X es criminal si es estadounidense y vendió un arma Z a una nación hostil Y.
